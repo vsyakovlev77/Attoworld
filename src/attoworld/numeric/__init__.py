7bf8e9f9cece7e75156ad5cf33a3a4643be4ce8f
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2025-05-26 17:00:11 +0200

    import atomic_units

--- src/attoworld/numeric/__init__.py
+++ src/attoworld/numeric/__init__.py
@@ -3,4 +3,5 @@ This module will contain numerical tools.
 """
 
 from .numeric import *
+from .atomic_units import *
 __all__ = [name for name in globals() if callable(globals()[name]) and not name.startswith("_")]
